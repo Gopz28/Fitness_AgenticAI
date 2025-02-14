5cb70dd3f62b82f5c793d10ae223d5a661331e2a
Gopz28 <djgopz28@gmail.com>
2025-02-14 22:22:40 +0530

    agnos_convertion

--- model.py
+++ model.py
@@ -1,20 +1,20 @@
-from phi.agent import Agent
-from phi.model.groq import Groq
-from phi.tools.duckduckgo import DuckDuckGo
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.duckduckgo import DuckDuckGoTools
 
 # Initialize the Legal Information Agent
-legal_agent = Agent(
+fitness_agent = Agent(
     name="Startup Law Agent",
     role="Provide legal information and procedures for startups",
     model=Groq(id="llama-3.3-70b-versatile"),
-    tools=[DuckDuckGo(search=True, news=True)],
+    tools=[DuckDuckGoTools(search=True, news=True)],
     instructions=["Always include sources", 
                 "Provide clear, structured responses", 
                 "Provide details only for Fitness and Dietinge", 
                 "if there is any requirement of tables generate a table view for better viewable format",
                 "Dont respond to the questions which are not related to Fitness and dieting instead of that give them a message of irrelevancy proffesionaly and guide them to ask the question about relavant problem",
                 "Respond only for Fitness and dieting sources if there is any chart for the prompt provide the chart ",
-                "if there are no relevant data about fitness Search it on the duckduckgo at phi tools"],
+                "if there are no relevant data about fitness Search it on the duckduckgotools at agno tools"],
     show_tool_calls=True,
     markdown=True
 )
@@ -35,7 +35,7 @@ def main():
         
         try:
             # Generate response from the legal agent
-            full_response = legal_agent.print_response(user_input, stream=True)  # Get full response at once
+            full_response = fitness_agent.print_response(user_input, stream=True)  # Get full response at once
             
             # Display the response
             print("\nAssistant's Response:")
